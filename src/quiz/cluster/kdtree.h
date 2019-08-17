bc108c701908f4f4391937a3de43a737f6d85727
JUNG Myoungki <myoungki.jung@gmail.com>
2019-08-17 11:06:18 +1000

    Feature: KD tree
    KDtree created
    
    TODO:
    more optimised using median to start with for root
    faster running searching time

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -27,11 +27,29 @@ struct KdTree
 
 	void insert(std::vector<float> point, int id)
 	{
-		// TODO: Fill in this function to insert a new point into the tree
-		// the function should create a new node and place correctly with in the root 
+		insertHelper(&root, 0, point, id);
 
 	}
 
+	void insertHelper(Node** node, uint depth, std::vector<float> point, int id)
+	{
+		//Case Empty Tree
+		if(* node == nullptr)
+		{
+			*node = new Node(point, id);
+		}
+		else
+		{
+			//even or odd?
+			uint cd = depth%2;
+
+			if(point[cd] < ((*node)->point[cd])) 
+				insertHelper( &((*node)->left),  depth+1, point, id);
+			else 
+				insertHelper( &((*node)->right), depth+1, point, id);
+		}
+			
+	}
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
