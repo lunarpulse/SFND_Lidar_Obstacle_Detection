7c085d8254ecaf1135c26792a52509bca55385d3
JUNG Myoungki <myoungki.jung@gmail.com>
2019-08-17 18:30:52 +1000

    Feature: stream PCD

--- src/environment.cpp
+++ src/environment.cpp
@@ -85,10 +85,6 @@ void initCamera(CameraAngle setAngle, pcl::visualization::PCLVisualizer::Ptr& vi
 
 void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer)
 {
-  // ----------------------------------------------------
-  // -----Open 3D viewer and display City Block     -----
-  // ----------------------------------------------------
-
   ProcessPointClouds<pcl::PointXYZI>* pointProcessorI = new ProcessPointClouds<pcl::PointXYZI>();
   pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloud = pointProcessorI->loadPcd("/home/lunarpulse/Documents/SensorFusion/SFND_Lidar_Obstacle_Detection/src/sensors/data/pcd/data_1/0000000000.pcd");
   pcl::PointCloud<pcl::PointXYZI>::Ptr filteredCloud = pointProcessorI->FilterCloud(inputCloud, .2,
@@ -101,17 +97,49 @@ void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer)
     auto clusters = pointProcessorI->Clustering(segmentCloud.first, 0.5, 40, 25000);
     uint clusterID = 0;
     std::vector<Color> colours = {Color(0.5,0,0), Color(0., 0.5,0),Color(0.0,0,0.5),
-                            Color(0.5,0.5,0),Color(0.,0.5,0.5),Color(0.5,0,0.5)
-                        ,Color(1.0,0,0.5),Color(0.5,1.0, 0.0),Color(0.5,0,1.0)
-                        ,Color(1.0,0.5,0.5),Color(0.5,1.0, 0.5),Color(0.5,0.5,1.0)
-                        ,Color(1.0,0.5,0.5),Color(0.5,1.0, 0.5),Color(0.5,0.5,1.0)
-                        ,Color(1.0,1.0,0.5),Color(0.5,1.0, 1.0),Color(1.0,0.5,1.0) };
+                                Color(0.5,0.5,0),Color(0.,0.5,0.5),Color(0.5,0,0.5)
+                                ,Color(1.0,0,0.5),Color(0.5,1.0, 0.0),Color(0.5,0,1.0)
+                                ,Color(1.0,0.5,0.5),Color(0.5,1.0, 0.5),Color(0.5,0.5,1.0)
+                                ,Color(1.0,0.5,0.5),Color(0.5,1.0, 0.5),Color(0.5,0.5,1.0)
+                                ,Color(1.0,1.0,0.5),Color(0.5,1.0, 1.0),Color(1.0,0.5,1.0) };
 
     for (auto &&cluster : clusters)
     {
         pointProcessorI->numPoints(cluster);
         renderPointCloud(viewer, cluster, "obsCloud"+std::to_string(clusterID), colours[clusterID%colours.size()]);
-        renderBox( viewer, pointProcessorI->BoundingBox(cluster), clusterID, Color(0,.50,0.5), 0.125f);
+        renderBox( viewer, pointProcessorI->BoundingBox(cluster), clusterID,
+         Color(1 - colours[clusterID%colours.size()].r,
+         1 - colours[clusterID%colours.size()].b,
+         1- 1 - colours[clusterID%colours.size()].g), 0.25f);
+        clusterID++;
+    }
+}
+
+void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer, ProcessPointClouds<pcl::PointXYZI>* pointProcessorI, const pcl::PointCloud<pcl::PointXYZI>::Ptr& inputCloud){
+    pcl::PointCloud<pcl::PointXYZI>::Ptr filteredCloud = pointProcessorI->FilterCloud(inputCloud, .2,
+        Eigen::Vector4f(-10.0, -6.0, -1.0, 1.0), Eigen::Vector4f(30.0, 6.0, 2.0, 1.0));
+    std::pair<pcl::PointCloud<pcl::PointXYZI>::Ptr, pcl::PointCloud<pcl::PointXYZI>::Ptr> segmentCloud =
+        pointProcessorI->SegmentPlane(filteredCloud, 100, 0.2);
+    renderPointCloud(viewer,segmentCloud.second,"planeCloud",Color(0,1,0));
+
+    //renderPointCloud(viewer,segmentCloud.first,"obstCloud",Color(1,0,0));
+    auto clusters = pointProcessorI->Clustering(segmentCloud.first, 0.5, 40, 25000);
+    uint clusterID = 0;
+    std::vector<Color> colours = {Color(0.5,0,0), Color(0., 0.5,0),Color(0.0,0,0.5),
+                                Color(0.5,0.5,0),Color(0.,0.5,0.5),Color(0.5,0,0.5)
+                                ,Color(1.0,0,0.5),Color(0.5,1.0, 0.0),Color(0.5,0,1.0)
+                                ,Color(1.0,0.5,0.5),Color(0.5,1.0, 0.5),Color(0.5,0.5,1.0)
+                                ,Color(1.0,0.5,0.5),Color(0.5,1.0, 0.5),Color(0.5,0.5,1.0)
+                                ,Color(1.0,1.0,0.5),Color(0.5,1.0, 1.0),Color(1.0,0.5,1.0) };
+
+    for (auto &&cluster : clusters)
+    {
+        pointProcessorI->numPoints(cluster);
+        renderPointCloud(viewer, cluster, "obsCloud"+std::to_string(clusterID), colours[clusterID%colours.size()]);
+        renderBox( viewer, pointProcessorI->BoundingBox(cluster), clusterID,
+            Color(1 - colours[clusterID%colours.size()].r,
+            1 - colours[clusterID%colours.size()].b,
+            1- 1 - colours[clusterID%colours.size()].g), 0.25f);
         clusterID++;
     }
 }
@@ -123,11 +151,25 @@ int main (int argc, char** argv)
     pcl::visualization::PCLVisualizer::Ptr viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
     CameraAngle setAngle = XY;
     initCamera(setAngle, viewer);
-    //simpleHighway(viewer);
-    cityBlock(viewer);
+    ProcessPointClouds<pcl::PointXYZI>* pointProcessorI = new ProcessPointClouds<pcl::PointXYZI>();
+    std::vector<boost::filesystem::path> stream = pointProcessorI->streamPcd("/home/lunarpulse/Documents/SensorFusion/SFND_Lidar_Obstacle_Detection/src/sensors/data/pcd/data_1");
+    auto streamIterator = stream.begin();
+    pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloudI;
 
     while (!viewer->wasStopped ())
     {
+        // Clear viewer
+        viewer->removeAllPointClouds();
+        viewer->removeAllShapes();
+
+        // Load pcd and run obstacle detection process
+        inputCloudI = pointProcessorI->loadPcd((*streamIterator).string());
+        cityBlock(viewer, pointProcessorI, inputCloudI);
+
+        streamIterator++;
+        if(streamIterator == stream.end())
+        streamIterator = stream.begin();
+
         viewer->spinOnce ();
     } 
 }
